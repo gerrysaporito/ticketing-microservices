bf3c8b50f415eefd542f5b94f1e151714731bee6
Gerry Saporito <gerrysaporito@gmail.com>
2021-05-27 14:35:40 -0400

    Added startup messages

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
+  console.log('starting up');
   if(!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
   }
